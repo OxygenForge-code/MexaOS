e696e1fc05ebe8379fc35e5b5477fe4f697e1d73
OxygenForge-code <oxygenforge@gmail.com>
2026-07-09 20:22:03 +0300

    Update kernlib.h

--- kernel/include/kernlib.h
+++ kernel/include/kernlib.h
@@ -1,46 +1,19 @@
 /* ============================================================
  *  MexaOS Kernel Library Header
- *  Common utilities used across kernel modules
  * ============================================================ */
 
-#ifndef KERNLIB_H
-#define KERNLIB_H
+#ifndef MEXAOS_KERNLIB_H
+#define MEXAOS_KERNLIB_H
 
-#include "../../include/mexaos.h"
+#include <stdint.h>
 
-/* Multiboot2 tag types */
-struct multiboot_tag {
-    uint32_t type;
-    uint32_t size;
-};
-
-struct multiboot_tag_string {
-    uint32_t type;
-    uint32_t size;
-    char string[];
-};
-
-struct multiboot_tag_framebuffer {
-    uint32_t type;
-    uint32_t size;
-    uint64_t addr;
-    uint32_t pitch;
-    uint32_t width;
-    uint32_t height;
-    uint8_t  bpp;
-    uint8_t  framebuffer_type;
-    uint16_t reserved;
-};
-
-/* Boot info parsing */
+/* Boot info parser */
 void parse_boot_info(uint64_t addr);
-void parse_memory_map(struct multiboot_tag *tag);
 
-/* Kernel functions */
-void kernel_idle(void);
-void kprintf(const char *fmt, ...);
-void __attribute__((noreturn)) kpanic(const char *msg, const char *file, int line);
+/* Kernel panic - noreturn */
+void __attribute__((noreturn)) kpanic_impl(const char *msg, const char *file, int line);
 
-#define PANIC(msg) kpanic(msg, __FILE__, __LINE__)
+/* Convenience macro */
+#define kpanic(msg) kpanic_impl(msg, __FILE__, __LINE__)
 
-#endif /* KERNLIB_H */
+#endif
